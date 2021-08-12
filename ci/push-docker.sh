9f137e893d5076377d68265be793f9296b5f0e90
Paulina Dubas <paulina.dubas@eficode.com>
2021-08-12 15:53:49 +0200

    .

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+echo "$docker_password" | docker login --username "$docker_username" --password-stdin
 docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::4}" 
 docker push "$docker_username/micronaut-app:latest" &
 wait
