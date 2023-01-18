7e939bf293c4fbcf29933f870ebd7d4285b8d511
Samuel Enderwitz <samuel.enderwitz@eficode.com>
2023-01-18 13:12:11 +0100

    use github packages

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username}/"
 docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "$docker_username/micronaut-app:latest" &
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
+docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/micronaut-app:latest" &
 wait
