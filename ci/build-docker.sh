8525cb0893bd4435d08323f5799d80a55285379f
Orlando <orlando.garcia@gmail.com>
2023-01-19 13:55:38 +0100

    tag

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username}/"
 docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
