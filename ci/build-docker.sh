a9404987037c1afd1d7e2bac340119c1a74dde91
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2024-01-31 10:22:51 +0100

    Changed how ci scripts takes envs

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
+REPO="ghcr.io/$GITHUB_ACTOR/"
+echo "${REPO}"
+docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
