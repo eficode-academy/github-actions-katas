fbae9035c18a5c2d14aa7c975b2258bccaab0b5c
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2024-02-07 09:48:45 +0100

    Update Docker repository username in build and push scripts

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-REPO="ghcr.io/$GITHUB_ACTOR/"
+REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
