7ef2a46d5e566c2e119f9ed72aa0943447ecb106
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2024-08-14 10:47:58 +0200

    changed docker-compose to the new syntax docker compose

--- ci/component-test.sh
+++ ci/component-test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker_username=$DockerRepo docker-compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+docker_username=$DockerRepo docker compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
